f16348d036d9a80253f32ebd1f0a82b1fe6b3d11
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-10 19:34:10 +0700

    tugas 15 js

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+var AB = 8
+var BC = 6
+
+console.log("Nilai AB : ", AB)
+console.log("Nilai BC : ", BC)
+
+var AC = Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2)) 
+
+
+
+console.log("panjang sisi AC pada segitiga siku-siku tersebut adalah :", AC, "cm")
